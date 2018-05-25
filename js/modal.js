0596c0f1234245fe9cdca742f882dc87aaab8fd8
Mark Jeremiah Robert <Mark Jeremiah@markjrobby.(none)>
2018-05-24 23:11:54 -0400

    add modal functionality for all CTA

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,20 @@
 window.onload = function(){
     var modal = document.getElementById('myModal'); // Get the modal
     var btn = document.getElementById("myBtn"); // Get the button that opens the modal
+    var btn2 = document.getElementById("myBtn2"); // Get the button that opens the modal
+    var btn3 = document.getElementById("myBtn3"); // Get the button that opens the modal
     var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 
     // When the user clicks the button, open the modal 
     btn.onclick = function() { 
+        modal.style.display = "block";
+    }
+
+    btn2.onclick = function() { 
+        modal.style.display = "block";
+    }
+
+    btn3.onclick = function() { 
         modal.style.display = "block";
     }
 
