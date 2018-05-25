7ba67e7a420c3215652a498246b8b80cd150d376
AgoloMarx <marx.low@agolo.com>
2018-05-25 17:52:15 -0400

    fix CTA access button

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,29 @@
-window.onload = function(){
+window.onload = function () {
     var modal = document.getElementById('myModal'); // Get the modal
-    var btn = document.getElementById("myBtn"); // Get the button that opens the modal
-    var btn2 = document.getElementById("myBtn2"); // Get the button that opens the modal
-    var btn3 = document.getElementById("myBtn3"); // Get the button that opens the modal
+    var desktopNavBtn = document.getElementById("desktop-nav-btn");
+    var mobileNavBtn = document.getElementById("mobile-nav-btn"); // Get the button that opens the modal
+    var btn2 = document.getElementById("myBtn2");
+    var btn3 = document.getElementById("myBtn3");
     var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() { 
+    // When the user clicks the button, open the modal
+    const openModal = function () {
         modal.style.display = "block";
     }
 
-    btn2.onclick = function() { 
+    desktopNavBtn.onclick = openModal;
+    mobileNavBtn.onclick = openModal;
+
+    btn2.onclick = function () {
         modal.style.display = "block";
     }
 
-    btn3.onclick = function() { 
+    btn3.onclick = function () {
         modal.style.display = "block";
     }
 
     // When the user clicks on "Sorry, I'm not interested, close the modal
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
 }
